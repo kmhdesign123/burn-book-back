bebbf2875e06a3fe6500d5962727a55612e9feb5
kmhdesign123 <kmhenson.design@gmail.com>
2023-06-06 20:13:49 -0500

    update profile model

--- migrations/20230606231440-create-vote.js
+++ migrations/20230606231440-create-vote.js
@@ -10,13 +10,29 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       value: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        validate: {
+          min: 0,
+          max: 5,
+        },
       },
       profileId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
       },
       voterId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
       },
       createdAt: {
         allowNull: false,
