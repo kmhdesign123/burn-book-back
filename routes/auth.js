9f4a9a66e42e58132b0ea5ae19444db3bf1b18fd
kmhdesign123 <kmhenson.design@gmail.com>
2023-06-06 21:35:47 -0500

    add delete account route in the auth router

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,6 @@ router.post('/login', authCtrl.login)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.post('/change-password', checkAuth, authCtrl.changePassword)
+router.delete('/delete', checkAuth, authCtrl.deleteAccount)
 
 module.exports = router
